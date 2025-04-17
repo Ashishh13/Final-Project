deef7b2e98c215143186b1bb047d814467147313
Ashishh13 <itsmeashishsingh13@gmail.com>
2025-04-18 02:23:19 +0530

    Initial commit

---
+++ django_app/items/admin.py
@@ -0,0 +1,6 @@
+# items/admin.py
+from django.contrib import admin
+from .models import Item
+
+admin.site.register(Item)
+
---
+++ django_app/items/views.py
@@ -0,0 +1,12 @@
+# items/views.py
+from django.shortcuts import render, redirect
+from .models import Item
+
+def home(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        if name:
+            Item.objects.create(name=name)
+            return redirect('/')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
